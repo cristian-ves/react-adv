152ebddd8bf4c62b7c8896ed103e8e3a2f4ab121
alejandro <alejandrovasquezesc@gmail.com>
2024-10-30 21:50:55 -0600

    added lazyloading to routes

--- src/01-lazyload/routes/routes.ts
+++ src/01-lazyload/routes/routes.ts
@@ -1,28 +1,33 @@
-import { LazyPage1, LazyPage2, LazyPage3 } from "../pages";
+import { LazyExoticComponent, lazy } from 'react';
 
-// type JSXComponent = () => JSX.Element;
+type JSXComponent = () => JSX.Element;
+
 
 interface Route {
     path: string;
-    Component: () => JSX.Element;
+    component: LazyExoticComponent< JSXComponent > | JSXComponent;
     name: string;
     children?: Route[];
 }
 
+const LazyPage1 = lazy(() => import('../pages/LazyPage1'));
+const LazyPage2 = lazy(() => import('../pages/LazyPage2'));
+const LazyPage3 = lazy(() => import('../pages/LazyPage3'));
+
 export const routes: Route[] = [
     {
         path: '/lazy1',
-        Component: LazyPage1,
+        component: LazyPage1,
         name: "LazyPage-1"
     },
     {
         path: '/lazy2',
-        Component: LazyPage2,
+        component: LazyPage2,
         name: "LazyPage-2"
     },
     {
         path: '/laz3',
-        Component: LazyPage3,
+        component: LazyPage3,
         name: "LazyPage-3"
     },
 ]
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink
+  NavLink,
+  Redirect
 } from "react-router-dom";
 
 import logo from '../logo.svg';
@@ -12,27 +13,32 @@ import { routes } from "../01-lazyload/routes/routes";
 
 export const Navigation = () => {
   return (
-    <Router>
-      <div className="main-layout">
-        <nav>
-          <img src={logo} alt="React logo" />
-          <ul>
-            {routes.map(({path, name}) => (
-              <li>
-                <NavLink to={path} activeClassName="nav-active" exact>{name}</NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
+    <Suspense fallback={<span>loading...</span>}>
+      <Router>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React logo" />
+            <ul>
+              {routes.map(({ path, name }) => (
+                <li key={path}>
+                  <NavLink to={path} activeClassName="nav-active" exact>{name}</NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
 
-        <Switch>
-          {routes.map(({path, Component}) => (
-            <Route path={path}>
-              <Component/>
-            </Route>
-          ))}
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            {routes.map(({ path, component: Component }) => (
+              <Route path={path} key={path}>
+                <Component />
+              </Route>
+            ))}
+
+            <Redirect to={routes[0].path} />
+
+          </Switch>
+        </div>
+      </Router>
+    </Suspense>
   );
 }
