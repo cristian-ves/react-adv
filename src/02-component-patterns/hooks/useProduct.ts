2a3db1fdd926730a784ed811600cd60e38e0888f
aleja <alejandrovasquezesc@gmail.com>
2026-06-08 23:04:07 -0600

    Control Props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -17,10 +17,6 @@ export const useProduct = ({
     const isControlled = useRef(!!onChange);
 
     const increaseBy = (value: number) => {
-        if (isControlled.current) {
-            return onChange!({ count: value, product });
-        }
-
         const newValue = Math.max(counter + value, 0);
         setCounter(newValue);
 
